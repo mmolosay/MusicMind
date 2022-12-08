394ea70d068e772449be53397b1588cab56a4dd1
mmolosay <mmolosays@gmail.com>
2022-12-08 16:34:13 +0300

    Initial push

---
+++ src/jvmMain/kotlin/ui/Application.kt
@@ -0,0 +1,26 @@
+package ui
+
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+/**
+ * Root component of whole application's UI.
+ */
+@Composable
+fun MTheoryApplication() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
