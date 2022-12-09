87ce05b07faf22dcfe639562654a86667b6d98ff
mmolosay <mmolosays@gmail.com>
2022-12-09 17:10:38 +0300

    Introduce 'desktop' module

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.mmolosay"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        withJava()
+    }
+    sourceSets {
+        @Suppress("UNUSED_VARIABLE")
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "MTheory"
+            packageVersion = "1.0.0"
+        }
+    }
+}
