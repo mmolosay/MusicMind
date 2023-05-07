26398dadf2a25f21eb420d7cdd020a7f6c648e5c
mmolosay <mmolosays@gmail.com>
2023-05-07 02:36:27 +0200

    Add some more extensions

--- shared/src/commonMain/kotlin/io/github/mmolosay/musicmind/theory/pitch/Pitch.kt
+++ shared/src/commonMain/kotlin/io/github/mmolosay/musicmind/theory/pitch/Pitch.kt
@@ -2,17 +2,25 @@ package io.github.mmolosay.musicmind.theory.pitch
 
 import io.github.mmolosay.musicmind.theory.Constants
 import kotlin.math.abs
+import kotlin.math.absoluteValue
 import kotlin.math.ln
+import kotlin.math.pow
 import kotlin.math.round
 
 /**
  * Pitch is a note without a duration; a percievable frequency of sound.
+ *
+ * Though there's really no sense in creating pitches that are not percievable by human, you may do so.
  *
  * [Pitch](https://en.wikipedia.org/wiki/Pitch_(music))
  * [Tone](https://en.wikipedia.org/wiki/Musical_tone)
  */
 @JvmInline
 value class Pitch internal constructor(val frequency: Double) : Comparable<Pitch> {
+
+    init {
+        require(frequency > 0) { "Pitch can only be defined by positive frequency" }
+    }
 
     override fun compareTo(other: Pitch): Int =
         frequency.compareTo(other.frequency)
@@ -26,7 +34,12 @@ value class Pitch internal constructor(val frequency: Double) : Comparable<Pitch
     operator fun div(other: Pitch): Double =
         this.frequency / other.frequency
 
+    operator fun div(other: Double): Pitch =
+        Pitch(frequency / other)
+
     companion object {
+        val HumanPercievableLowest by lazy { 20.hz }
+        val HumanPercievableHighest by lazy { 20_000.hz }
         val InfinitelyLow by lazy { Pitch(Double.MIN_VALUE) }
         val InfinitelyHigh by lazy { Pitch(Double.MAX_VALUE) }
     }
@@ -41,11 +54,22 @@ val Int.hz: Pitch
 val Pitch.octaveHigher: Pitch
     get() = this * Constants.OctaveRatio
 
+val Pitch.isHumanPercievable: Boolean
+    get() = this >= Pitch.HumanPercievableLowest && this <= Pitch.HumanPercievableHighest
+
+fun Pitch.transpose(octaves: Int): Pitch =
+    if (octaves == 0) this
+    else this * Constants.OctaveRatio.pow(octaves)
+
+infix fun Pitch.isPercievablyEqualTo(other: Pitch): Boolean =
+    (this.frequency - other.frequency).absoluteValue < PitchEqualityThreshold
+
 infix fun Pitch.isOfSamePitchClassAs(other: Pitch): Boolean {
     val ratio = other / this
     val log2ratio = ln(ratio) / Log2
     val diff = abs(round(log2ratio) - log2ratio)
-    return (diff < 2e-5)
+    return (diff < PitchEqualityThreshold)
 }
 
+private const val PitchEqualityThreshold = 2e-5
 private val Log2 = ln(2.0)
