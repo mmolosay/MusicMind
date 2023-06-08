a9c550d007572b0d331e7f80cd4e614e93175a59
mmolosay <mmolosays@gmail.com>
2023-06-08 12:32:01 +0200

    Move 'InstrumentKey' declaration

--- shared/src/commonMain/kotlin/io/github/mmolosay/musicmind/theory/instruments/discrete/keys/InstrumentKeys.kt
+++ shared/src/commonMain/kotlin/io/github/mmolosay/musicmind/theory/instruments/discrete/keys/InstrumentKeys.kt
@@ -15,3 +15,20 @@ sealed class InstrumentKeys<K : InstrumentKey> {
 
 fun <K : InstrumentKey> InstrumentKeys<K>.withOrNull(ordinal: Int): K? =
     if (!contains(ordinal)) null else with(ordinal)
+
+/**
+ * Represents a specific physical position on a discrete pitch instrument.
+ * It is a key on a piano and fret of string on a guitar.
+ *
+ * Its [ordinal] property contains absolute ordinal of this key among other keys of this instrument.
+ * So, keyboard key with ordinal = 49 would be exactly 49th key of this instrument.
+ *
+ * In terms of one instrument, each key has a unique [ordinal].
+ */
+sealed interface InstrumentKey : Comparable<InstrumentKey> {
+
+    val ordinal: Int
+
+    override fun compareTo(other: InstrumentKey): Int =
+        this.ordinal.compareTo(other.ordinal)
+}
