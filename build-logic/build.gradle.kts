f19b58cc842068231a862adcbe47c07598efbcf9
mmolosay <mmolosays@gmail.com>
2023-06-10 18:24:55 +0200

    Slight adjustments

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,16 +5,21 @@ plugins {
 group = "io.github.mmolosay"
 
 repositories {
+    mavenCentral()
     mavenLocal()
     google()
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    gradlePluginPortal()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        name = "Compose for Desktop DEV"
+    }
 }
 
 // TODO: change to compileOnly
 // TODO: use versions catalog
+// https://github.com/JetBrains/compose-multiplatform/blob/master/VERSIONING.md#kotlin-compatibility
 dependencies {
     // TODO: update versions
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.1")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.1") // id("org.jetbrains.compose")
 }
