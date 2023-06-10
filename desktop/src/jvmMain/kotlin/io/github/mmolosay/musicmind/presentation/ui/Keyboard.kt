a424d624e6905e2339f40d6b5a3f433199092a1b
mmolosay <mmolosays@gmail.com>
2023-06-10 18:25:09 +0200

    Draw key labels

--- desktop/src/jvmMain/kotlin/io/github/mmolosay/musicmind/presentation/ui/Keyboard.kt
+++ desktop/src/jvmMain/kotlin/io/github/mmolosay/musicmind/presentation/ui/Keyboard.kt
@@ -8,13 +8,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.BoxWithConstraintsScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
@@ -23,12 +28,11 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults.Colors
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults.DrawLabel
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults.DrawLabels
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults.NaturalsSpacing
 import io.github.mmolosay.musicmind.presentation.ui.design.MusicMindTheme
-
-data class NaturalsSpacing(
-    val naturalKeyWidthFraction: Float = 0.04f,
-    val max: Dp = 6.dp,
-)
 
 @Composable
 fun Keyboard(
@@ -36,11 +40,8 @@ fun Keyboard(
     octaves: Int,
     onNaturalKeyClick: () -> Unit,
     onAccidentalKeyClick: () -> Unit,
-    drawLabels: Boolean = true,
-    naturalsColor: Color = MusicMindTheme.colors.keyboardNaturals,
-    naturalsIndicationColor: Color = MusicMindTheme.colors.keyboardNaturalsIndication,
-    accidentalsColor: Color = MusicMindTheme.colors.keyboardAccidentals,
-    accidentalsIndicationColor: Color = MusicMindTheme.colors.keyboardAccidentalsIndication,
+    drawLabels: DrawLabels = DrawLabels.No,
+    colors: Colors = KeyboardDefaults.colors(),
     spacing: NaturalsSpacing = NaturalsSpacing(),
 ) {
     BoxWithConstraints(
@@ -73,11 +74,16 @@ fun Keyboard(
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(space),
                     ) {
-                        val indication = rememberRipple(color = naturalsIndicationColor)
-                        repeat(naturals) {
+                        val indication = rememberRipple(color = colors.naturalsIndication)
+                        repeat(naturals) { i ->
+                            val drawLabel = when (drawLabels) {
+                                is DrawLabels.No -> DrawLabel.No
+                                is DrawLabels.Yes -> DrawLabel.Yes(drawLabels.naturals[i], colors.naturalsLabel)
+                            }
                             KeyboardKey(
-                                modifier = Modifier.size(naturalsSize),
-                                color = naturalsColor,
+                                size = naturalsSize,
+                                color = colors.naturals,
+                                drawLabel = drawLabel,
                                 indication = indication,
                                 onClick = onNaturalKeyClick,
                             )
@@ -86,15 +92,19 @@ fun Keyboard(
                     // Accidentals
                     var x = 0.dp
                     val offset = (accidentalsSize.width + space) / 2f
-                    val indication = rememberRipple(color = accidentalsIndicationColor)
+                    val indication = rememberRipple(color = colors.accidentalsIndication)
                     repeat(accidentals) { i ->
+                        val drawLabel = when (drawLabels) {
+                            is DrawLabels.No -> DrawLabel.No
+                            is DrawLabels.Yes -> DrawLabel.Yes(drawLabels.accidentals[i], colors.accidentalsLabel)
+                        }
                         val dist = NaturalsDistances[i]
                         x += (naturalsSize.width + space) * dist
                         KeyboardKey(
-                            modifier = Modifier
-                                .size(accidentalsSize)
-                                .offset(x = x - offset),
-                            color = accidentalsColor,
+                            modifier = Modifier.offset(x = x - offset),
+                            size = accidentalsSize,
+                            color = colors.accidentals,
+                            drawLabel = drawLabel,
                             indication = indication,
                             onClick = onAccidentalKeyClick,
                         )
@@ -134,17 +144,18 @@ private fun BoxWithConstraintsScope.calcKeyboardSizeFromHeight(octaves: Int): Dp
 
 @Composable
 private fun KeyboardKey(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
+    size: DpSize,
     color: Color,
     cornerRadius: Dp = 3.dp,
+    drawLabel: DrawLabel,
     indication: Indication,
     onClick: () -> Unit,
 ) {
-    // TODO: add 'drawLabels' Boolean parameter and draw labels
     val density = LocalDensity.current
     val radius = with(density) { CornerRadius(x = cornerRadius.toPx()) }
     val shape = RoundedCornerShape(cornerRadius)
-    Canvas(
+    Box(
         modifier = modifier
             .clip(shape)
             .clickable(
@@ -152,11 +163,65 @@ private fun KeyboardKey(
                 indication = indication,
                 onClick = onClick,
             ),
+        contentAlignment = Alignment.BottomCenter,
     ) {
-        drawRoundRect(
-            color = color,
-            cornerRadius = radius,
+        Canvas(modifier = Modifier.size(size)) {
+            drawRoundRect(
+                color = color,
+                cornerRadius = radius,
+            )
+        }
+        if (drawLabel is DrawLabel.Yes) {
+            Column {
+                Text(text = drawLabel.label, color = drawLabel.color)
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
+    }
+}
+
+object KeyboardDefaults {
+
+    @Composable
+    fun colors(
+        naturals: Color = MusicMindTheme.colors.keyboardNaturals,
+        naturalsLabel: Color = Color.LightGray, // TODO
+        naturalsIndication: Color = MusicMindTheme.colors.keyboardNaturalsIndication,
+        accidentals: Color = MusicMindTheme.colors.keyboardAccidentals,
+        accidentalsLabel: Color = Color.LightGray, // TODO
+        accidentalsIndication: Color = MusicMindTheme.colors.keyboardAccidentalsIndication,
+    ): Colors =
+        Colors(
+            naturals = naturals,
+            naturalsLabel = naturalsLabel,
+            naturalsIndication = naturalsIndication,
+            accidentals = accidentals,
+            accidentalsLabel = accidentalsLabel,
+            accidentalsIndication = accidentalsIndication,
         )
+
+    data class Colors constructor(
+        val naturals: Color,
+        val naturalsLabel: Color,
+        val naturalsIndication: Color,
+        val accidentals: Color,
+        val accidentalsLabel: Color,
+        val accidentalsIndication: Color,
+    )
+
+    data class NaturalsSpacing(
+        val naturalKeyWidthFraction: Float = 0.04f,
+        val max: Dp = 6.dp,
+    )
+
+    sealed interface DrawLabels {
+        object No : DrawLabels
+        data class Yes(val naturals: List<String>, val accidentals: List<String>) : DrawLabels
+    }
+
+    sealed interface DrawLabel {
+        object No : DrawLabel
+        data class Yes(val label: String, val color: Color) : DrawLabel
     }
 }
 
--- desktop/src/jvmMain/kotlin/io/github/mmolosay/musicmind/presentation/ui/screen/InstrumentScreen.kt
+++ desktop/src/jvmMain/kotlin/io/github/mmolosay/musicmind/presentation/ui/screen/InstrumentScreen.kt
@@ -11,11 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.github.mmolosay.musicmind.presentation.DefaultNoteFormatter
 import io.github.mmolosay.musicmind.presentation.Strings
 import io.github.mmolosay.musicmind.presentation.resources.localizedString
 import io.github.mmolosay.musicmind.presentation.ui.Keyboard
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults
+import io.github.mmolosay.musicmind.presentation.ui.KeyboardDefaults.DrawLabels
 import io.github.mmolosay.musicmind.presentation.ui.components.Screen
 import io.github.mmolosay.musicmind.theory.context.MusicContext
+import io.github.mmolosay.musicmind.theory.instruments.discrete.keys.accidentals
+import io.github.mmolosay.musicmind.theory.instruments.discrete.keys.naturals
+import io.github.mmolosay.musicmind.theory.instruments.discrete.keys.range
+import io.github.mmolosay.musicmind.theory.tuning.instrument.KeyboardTuning
 
 @Composable
 fun InstrumentScreen() =
@@ -44,12 +51,31 @@ private fun LazyListScope.keyboard() =
     item(
         key = "keyboard",
     ) {
-        val piano = remember { MusicContext { Piano() } }
+        val tuning = KeyboardTuning(466.16376f)
+        val context = remember { MusicContext { Piano(instrumentTuning = tuning) } }
+        val labelFormatter = remember { DefaultNoteFormatter() }
+
+        val octaves = 2
+        val instrument = context.instrument
+        val keysPerOctave = instrument.tuningSystem.pitchClasses
+        val totalKeys = keysPerOctave * octaves
+
+        val keys = remember(context) { instrument.keys.range(28, totalKeys) }
+        val naturals = remember(context) {
+            keys.naturals()
+                .map { with(instrument) { it.label } }
+                .map { with(labelFormatter) { it.display } }
+        }
+        val accidentals = remember(context) {
+            keys.accidentals().first() // take only accidentals of rank=1
+                .map { with(instrument) { it.label } }
+                .map { with(labelFormatter) { it.display } }
+        }
         Keyboard(
-            modifier = Modifier
-                .height(400.dp),
-            octaves = 2,
+            modifier = Modifier.height(400.dp),
+            octaves = octaves,
             onNaturalKeyClick = { println("White key!") },
             onAccidentalKeyClick = { println("Black key!") },
+            drawLabels = DrawLabels.Yes(naturals, accidentals)
         )
     }
