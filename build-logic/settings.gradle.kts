28bb83bfb2a75c7b64e1e42dedff940c63f71f36
mmolosay <mmolosays@gmail.com>
2023-11-12 20:32:52 +0100

    Ger rid of "libs.versions.toml"

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -4,12 +4,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
 }
 
 rootProject.name = "build-logic"
